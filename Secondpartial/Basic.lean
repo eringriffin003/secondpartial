2edbd66b20e370302e58b70e3ac2a0820c9c4f34
Bjørn Kjos-Hanssen <bjoernkh@hawaii.edu>
2025-06-26 22:18:48 -0400

    Adding Taylor polynomials

--- Secondpartial/Basic.lean
+++ Secondpartial/Basic.lean
@@ -58,224 +58,264 @@ lemma DM₀ {n:ℕ} (a b c : EuclideanSpace ℝ (Fin n)) :
   ext i j
   fin_cases i <;> simp
 
-noncomputable def theFun {n : ℕ} (f : EuclideanSpace ℝ (Fin n) → ℝ)
-  (x₀ : EuclideanSpace ℝ (Fin n))
-    (had :
-      ∀ (m : Fin 2 → EuclideanSpace ℝ (Fin n)) (i : Fin 2) (x y : EuclideanSpace ℝ (Fin n)),
-      (iteratedFDeriv ℝ 2 f x₀).toFun (Function.update m i (x + y)) =
-        (iteratedFDeriv ℝ 2 f x₀).toFun (Function.update m i x) +
-          (iteratedFDeriv ℝ 2 f x₀).toFun (Function.update m i y))
-    (hsm :
-      ∀ (m : Fin 2 → EuclideanSpace ℝ (Fin n)) (i : Fin 2) (c : ℝ) (x : EuclideanSpace ℝ (Fin n)),
-      (iteratedFDeriv ℝ 2 f x₀).toFun (Function.update m i (c • x)) =
-        c • (iteratedFDeriv ℝ 2 f x₀).toFun (Function.update m i x)) :
-    EuclideanSpace ℝ (Fin n) → EuclideanSpace ℝ (Fin n) →ₗ[ℝ] ℝ := by
-  let T := fun y => iteratedFDeriv ℝ 2 f x₀ y
-  intro a
-  exact {
-    toFun := fun b => T ![a,b] + T ![b,a]
-    map_add' := by
-        intro b c
-        have h1 := had ![a, b] 1 b c
-        have h2 := had ![b, a] 0 b c
-        repeat rw [DM₁] at h1
-        repeat rw [DM₀] at h2
-        have : T ![a, b + c] = T ![a, b] + T ![a, c] := h1
-        have : T ![b + c, a] = T ![b, a] + T ![c, a] := h2
-        linarith
+noncomputable def hessianLinearCompanion {n : ℕ} (f : EuclideanSpace ℝ (Fin n) → ℝ)
+    (x₀ : EuclideanSpace ℝ (Fin n)) :
+    EuclideanSpace ℝ (Fin n) → EuclideanSpace ℝ (Fin n) →ₗ[ℝ] ℝ := fun a => {
+    toFun := fun b => iteratedFDeriv ℝ 2 f x₀ ![a,b]
+                    + iteratedFDeriv ℝ 2 f x₀ ![b,a]
+    map_add' := fun b c => by
+      have had := (iteratedFDeriv ℝ 2 f x₀).map_update_add'
+      have h₀ := had ![b, a] 0 b c
+      have h₁ := had ![a, b] 1 b c
+      repeat simp [DM₁] at h₁; simp [DM₀] at h₀
+      linarith
     map_smul' := by
       intro m x
-      simp [T]
+      have hsm := (iteratedFDeriv ℝ 2 f x₀).map_update_smul'
       have h₀ := hsm ![x,a] 0 m x
-      repeat rw [DM₀] at h₀
-      simp at h₀
-      rw [h₀]
-      have := hsm ![a,x] 1 m x
-      repeat rw [DM₁] at this
-      simp at this
-      rw [this]
+      have h₁ := hsm ![a,x] 1 m x
+      repeat rw [DM₀] at h₀; rw [DM₁] at h₁
+      simp at h₀ h₁ ⊢
       linarith
   }
 
-noncomputable def hessianForm {n : ℕ} (f : EuclideanSpace ℝ (Fin n) → ℝ)
+noncomputable def hessianBilinearCompanion {n : ℕ} (f : EuclideanSpace ℝ (Fin n) → ℝ)
+    (x₀ : EuclideanSpace ℝ (Fin n)): LinearMap.BilinMap ℝ (EuclideanSpace ℝ (Fin n)) ℝ := {
+    toFun := hessianLinearCompanion _ _
+    map_add' := fun x y => by
+        have had := (iteratedFDeriv ℝ 2 f x₀).map_update_add'
+        ext i
+        have had₀ := had ![x,i] 0 x y
+        have had₁ := had ![i,i] 1 x y
+        repeat rw [DM₀] at had₀
+        repeat rw [DM₁] at had₁
+        simp [hessianLinearCompanion] at had₀ had₁ ⊢
+        linarith
+    map_smul' := fun m x => LinearMap.ext_iff.mpr <| fun x₁ => by
+        have hsm := (iteratedFDeriv ℝ 2 f x₀).map_update_smul'
+        have hsm₀ := hsm ![x,x₁] 0 m x
+        have hsm₁ := hsm ![x₁,x] 1 m x
+        repeat rw [DM₀] at hsm₀
+        repeat rw [DM₁] at hsm₁
+        simp [hessianLinearCompanion] at hsm₀ hsm₁ ⊢
+        linarith
+}
+
+noncomputable def hessianQuadraticMap {n : ℕ} (f : EuclideanSpace ℝ (Fin n) → ℝ)
     (x₀ : EuclideanSpace ℝ (Fin n)) :
-  QuadraticMap ℝ (EuclideanSpace ℝ (Fin n)) ℝ := by
-    let g := (iteratedFDeriv ℝ 2 f x₀)
-    have hsm :
-      ∀ (m : Fin 2 → EuclideanSpace ℝ (Fin n)) (i : Fin 2) (c : ℝ) (x : EuclideanSpace ℝ (Fin n)),
-      g.toFun (Function.update m i (c • x)) = c • g.toFun (Function.update m i x) :=
-      (iteratedFDeriv ℝ 2 f x₀).map_update_smul'
-
-    have had :
-      ∀ (m : Fin 2 → EuclideanSpace ℝ (Fin n)) (i : Fin 2) (x y : EuclideanSpace ℝ (Fin n)),
-      g.toFun (Function.update m i (x + y)) = g.toFun (Function.update m i x)
-    + g.toFun (Function.update m i y) := (iteratedFDeriv ℝ 2 f x₀).map_update_add'
-
-    let Q₁ := fun y => iteratedFDeriv ℝ 2 f x₀ ![y,y]
-    exact {
-    toFun := Q₁
+  QuadraticMap ℝ (EuclideanSpace ℝ (Fin n)) ℝ :=
+  {
+    toFun := fun y => iteratedFDeriv ℝ 2 f x₀ ![y,y]
     exists_companion' := by
-      use {
-        toFun := by
-          apply theFun <;> tauto
-        map_add' := by
-          intro x y
-          simp
-          let g := theFun f x₀ had hsm (x+y)
-          simp [theFun]
-          ext i
-          simp
-          have : (iteratedFDeriv ℝ 2 f x₀) ![x + y, i]  =
-                 (iteratedFDeriv ℝ 2 f x₀) ![x, i] +
-                 (iteratedFDeriv ℝ 2 f x₀) ![y, i] := by
-            have had₀ := had ![x,i] 0 x y
-            repeat rw [DM₀] at had₀
-            simp at had₀
-            exact had₀
-          have : (iteratedFDeriv ℝ 2 f x₀) ![i, x + y] =
-                 (iteratedFDeriv ℝ 2 f x₀) ![i, x] +
-                 (iteratedFDeriv ℝ 2 f x₀) ![i, y] := by
-            have had₁ := had ![i,i] 1 x y
-            repeat rw [DM₁] at had₁
-            simp at had₁
-            exact had₁
-          linarith
-        map_smul' := by
-          intro m x
-          simp
-          refine LinearMap.ext_iff.mpr ?_
-          intro x₁
-          simp
-          unfold theFun
-
-          have hsm₀ := hsm ![x,x₁] 0 m x
-          repeat rw [DM₀] at hsm₀
-          simp at hsm₀
-          simp
-          rw [hsm₀]
-          have hsm₁ := hsm ![x₁,x] 1 m x
-          repeat rw [DM₁] at hsm₁
-          simp at hsm₁
-          rw [hsm₁]
-          linarith
-      }
+      have hsm := (iteratedFDeriv ℝ 2 f x₀).map_update_smul'
+      have had := (iteratedFDeriv ℝ 2 f x₀).map_update_add'
+      use hessianBilinearCompanion f x₀
       intro x y
-      simp [Q₁]
-      unfold theFun
-      simp
       have had₀ := had ![x, x + y] 0 x y
-      repeat rw [DM₀] at had₀
-      simp at had₀
-      rw [had₀]
-
-      have : (iteratedFDeriv ℝ 2 f x₀) ![x, x + y] =
-                (iteratedFDeriv ℝ 2 f x₀) ![x, x] +
-                (iteratedFDeriv ℝ 2 f x₀) ![x, y] := by
-        have had₁ := had ![x,x] 1 x y
-        repeat rw [DM₁] at had₁
-        simp at had₁
-        exact had₁
-      have : (iteratedFDeriv ℝ 2 f x₀) ![y, x + y] =
-                (iteratedFDeriv ℝ 2 f x₀) ![y, x] +
-                (iteratedFDeriv ℝ 2 f x₀) ![y, y] := by
-        have had₁ := had ![y,x] 1 x y
-        repeat rw [DM₁] at had₁
-        simp at had₁
-        exact had₁
+      have had₁ := had ![x,x] 1 x y
+      have had₂ := had ![y,x] 1 x y
+      repeat rw [DM₀] at had₀; rw [DM₁] at had₁ had₂
+      simp [hessianLinearCompanion, hessianBilinearCompanion] at had₀ had₁ had₂ ⊢
       linarith
     toFun_smul := by
+      have hsm := (iteratedFDeriv ℝ 2 f x₀).map_update_smul'
+      have had := (iteratedFDeriv ℝ 2 f x₀).map_update_add'
       intro u v
-      simp [Q₁]
       have hsm₀ := hsm ![v, v] 0 u v
-      repeat rw [DM₀] at hsm₀
-      simp at hsm₀
-      rw [mul_assoc]
-      rw [← hsm₀]
       have hsm₁ := hsm ![u • v,v] 1 u v
-      repeat rw [DM₁] at hsm₁
-      convert hsm₁ using 1
+      repeat simp [DM₀] at hsm₀; simp [DM₁] at hsm₁
+      rw [smul_eq_mul, mul_assoc, ← hsm₀, hsm₁]
   }
 
 /-- A continuous multilinear map is, in particular, bilinear
 in the sense needed to consider the `IsCoercive` proposition. -/
-noncomputable def getbilin {n : ℕ} (g : ContinuousMultilinearMap ℝ
-    (fun _ : Fin 2 ↦ EuclideanSpace ℝ (Fin n)) ℝ) :
+noncomputable def continuousBilinearMap_of_continuousMultilinearMap {n : ℕ}
+    (g : ContinuousMultilinearMap ℝ (fun _ : Fin 2 ↦ EuclideanSpace ℝ (Fin n)) ℝ) :
     (EuclideanSpace ℝ (Fin n)) →L[ℝ] (EuclideanSpace ℝ (Fin n)) →L[ℝ] ℝ := {
-        toFun := fun x => {
-            toFun := fun y => g.toFun ![x,y]
-            map_add' := by
-                intro a b
-                have := g.map_update_add ![x,b] 1 a b
-                repeat rw [DM₁] at this
-                exact this
-            map_smul' := by
-                intro m a
-                have := g.map_update_smul ![x,a] 1 m a
-                repeat rw [DM₁] at this
-                exact this
-            cont := by
-                have := g.cont
-                refine Continuous.comp' this ?_
-                simp
-                refine Continuous.matrixVecCons ?_ ?_
-                · exact continuous_const
-                · refine Continuous.matrixVecCons ?_ ?_
-                  · exact continuous_id'
-                  · exact continuous_const
-        }
+    toFun := fun x => {
+        toFun := fun y => g.toFun ![x,y]
         map_add' := by
             intro a b
-            ext c
-            simp
-            have := g.map_update_add ![a,c] 0 a b
-            repeat rw [DM₀] at this
+            have := g.map_update_add ![x,b] 1 a b
+            repeat rw [DM₁] at this
             exact this
         map_smul' := by
-            intro c x
-            ext y
-            simp
-            have := g.map_update_smul ![x,y] 0 c x
-            repeat rw [DM₀] at this
+            intro m a
+            have := g.map_update_smul ![x,a] 1 m a
+            repeat rw [DM₁] at this
             exact this
-        cont := by
-            simp
-            have := g.cont
-            refine continuous_clm_apply.mpr ?_
-            intro x
-            simp
-            refine Continuous.comp' this ?_
-            simp
-            refine Continuous.matrixVecCons ?_ ?_
-            · exact continuous_id'
-            · exact continuous_const
+        cont := Continuous.comp' g.cont <| Continuous.matrixVecCons continuous_const
+                <| Continuous.matrixVecCons continuous_id' continuous_const
     }
+    map_add' := by
+        intro a b
+        ext c
+        have := g.map_update_add ![a,c] 0 a b
+        repeat rw [DM₀] at this
+        exact this
+    map_smul' := by
+        intro c x
+        ext y
+        have := g.map_update_smul ![x,y] 0 c x
+        repeat rw [DM₀] at this
+        exact this
+    cont := continuous_clm_apply.mpr fun x => Continuous.comp' g.cont
+        <| Continuous.matrixVecCons continuous_id' continuous_const
+}
 
 theorem second_partial_deriv_test_for_deg_two {n : ℕ}
     {f : EuclideanSpace ℝ (Fin n) → ℝ} {x₀ : EuclideanSpace ℝ (Fin n)}
-    (h : ∀ x, f x = f x₀ + inner ℝ (gradient f x₀) (x - x₀) + (1 / 2) * hessianForm f x₀ (x - x₀))
-    (h₀ : gradient f x₀ = 0) (hQQ : (hessianForm f x₀).PosDef) : IsLocalMin f x₀ := by
-  unfold IsLocalMin IsMinFilter
-  refine Filter.eventually_iff_exists_mem.mpr ?_
+    (h : ∀ x, f x = f x₀
+                     + inner ℝ (gradient f x₀) (x - x₀)
+                  + (1 / 2) * hessianQuadraticMap f x₀ (x - x₀))
+    (h₀ : gradient f x₀ = 0) (hQQ : (hessianQuadraticMap f x₀).PosDef) :
+    IsLocalMin f x₀ := Filter.eventually_iff_exists_mem.mpr <| by
   use Set.univ
   constructor
   · simp
   · intro y _
-    rw [h y]
-    rw [h₀]
+    rw [h y, h₀]
     simp
     exact QuadraticMap.PosDef.nonneg hQQ (y - x₀)
 
+noncomputable def higher_taylor_coeff {n : ℕ}
+    (f : EuclideanSpace ℝ (Fin n) → ℝ) (x₀ : EuclideanSpace ℝ (Fin n)) (k : ℕ) :=
+    fun x =>
+    (1 / Nat.factorial k) * (fun y => iteratedFDeriv ℝ k f x₀
+        fun i => Fin.repeat k ![y] ⟨i.1,by omega⟩) (x - x₀)
+
+noncomputable def higher_taylor {n : ℕ}
+    (f : EuclideanSpace ℝ (Fin n) → ℝ) (x₀ : EuclideanSpace ℝ (Fin n)) (k : ℕ) :
+    EuclideanSpace ℝ (Fin n) → ℝ := by
+    intro x
+    let h (i) := higher_taylor_coeff f x₀ i x
+    exact ∑ i ∈ Finset.range (k+1), h i
+
+theorem second_partial_deriv_test_for_deg_two' {n : ℕ}
+    {f : EuclideanSpace ℝ (Fin n) → ℝ} {x₀ : EuclideanSpace ℝ (Fin n)}
+    (h : ∀ x, f x
+      = (1 / Nat.factorial 0) * (fun y => iteratedFDeriv ℝ 0 f x₀ (Fin.repeat 0 ![y])) (x - x₀)
+      + (1 / Nat.factorial 1) * (fun y => iteratedFDeriv ℝ 1 f x₀ (Fin.repeat 1 ![y])) (x - x₀)
+      + (1 / Nat.factorial 2) * (fun y => iteratedFDeriv ℝ 2 f x₀ (Fin.repeat 2 ![y])) (x - x₀))
+    (h₀ : gradient f x₀ = 0) (hQQ : (hessianQuadraticMap f x₀).PosDef) :
+    IsLocalMin f x₀ := Filter.eventually_iff_exists_mem.mpr <| by
+  use Set.univ
+  constructor
+  · simp
+  · intro y _
+    have h : ∀ x, f x
+      = (1 / Nat.factorial 0) * iteratedFDeriv ℝ 0 f x₀ ![]
+      + (1 / Nat.factorial 1) * (fun y => iteratedFDeriv ℝ 1 f x₀ (Fin.repeat 1 ![y])) (x - x₀)
+      + (1 / Nat.factorial 2) * (fun y => iteratedFDeriv ℝ 2 f x₀ ![y, y]) (x - x₀)
+                  := by
+                  intro x
+                  convert h x using 2
+                  congr
+                  simp
+                  congr
+                  ext i j
+                  fin_cases i <;> simp
+    have h : ∀ x, f x = (1 / Nat.factorial 0) * iteratedFDeriv ℝ 0 f x₀ ![]
+                  + (1 / Nat.factorial 1) * iteratedFDeriv ℝ 1 f x₀ ![x - x₀]
+                  + (1 / Nat.factorial 2) * (fun y => iteratedFDeriv ℝ 2 f x₀ ![y, y]) (x - x₀)
+                  := by
+                  intro x
+                  convert h x using 2
+                  congr
+                  simp
+    have h₁ : inner ℝ (gradient f x₀) (y - x₀) = (fderiv ℝ f x₀) (y - x₀) := by
+        unfold gradient
+        simp
+
+    have h₂ : (iteratedFDeriv ℝ 1 f x₀) ![y - x₀] = (fderiv ℝ f x₀) (y - x₀) := by
+        simp
+    have h₃ := Eq.trans h₁ h₂.symm
+    have := h y
+    rw [← h₃] at this
+    simp only [iteratedFDeriv_zero_apply] at this
+
+    rw [h₀] at this
+    simp at this
+    rw [this]
+    simp
+    exact QuadraticMap.PosDef.nonneg hQQ (y - x₀)
+
+/-- If a multivariate function equals its second Taylor polynomial
+then the second derivative test holds for it.
+This includes basic examples in multivariable calculus such as
+z = y² - x², z = x² + y² and z = 1 - x² - y² + 7x - 9xy.
+-/
+theorem second_partial_deriv_test_for_deg_two'' {n : ℕ}
+    {f : EuclideanSpace ℝ (Fin n) → ℝ} {x₀ : EuclideanSpace ℝ (Fin n)}
+    (h : f = higher_taylor f x₀ 2)
+    (h₀ : gradient f x₀ = 0) (hQQ : (hessianQuadraticMap f x₀).PosDef) :
+    IsLocalMin f x₀ := by
+    exact @second_partial_deriv_test_for_deg_two' n f x₀ (by
+        intro x
+        nth_rewrite 1 [h]
+
+        simp [higher_taylor, higher_taylor_coeff]
+        rw [Finset.range_succ]
+        simp
+        rw [Finset.range_succ]
+        simp
+        suffices (2⁻¹ * (iteratedFDeriv ℝ 2 f x₀) fun i ↦ x - x₀) =
+        2⁻¹ * (iteratedFDeriv ℝ 2 f x₀) (Fin.repeat 2 ![x - x₀]) by linarith
+        congr
+        ext i j
+        fin_cases i <;> simp) h₀ hQQ
+
+/-!
+
+# WORK CONTAINING SORRIES
+
+-/
+
+example (a b c d : ℝ) (f : EuclideanSpace ℝ (Fin 2) → ℝ) (x₀ : EuclideanSpace ℝ (Fin 2))
+    (h : ∀ x y, f ![x, y] = 0) :
+    ∀ x, f x = f x₀ + inner ℝ (gradient f x₀) (x - x₀) + (1 / 2) * hessianQuadraticMap f x₀ (x - x₀)
+    := by
+
+    sorry
+
+
 example (a b c d : ℝ) (f : EuclideanSpace ℝ (Fin 2) → ℝ) (x₀ : EuclideanSpace ℝ (Fin 2))
     (h : ∀ x y, f ![x, y] = a * x ^ 2 + b * x * y + c * y ^ 2 + d) :
-    ∀ x, f x = f x₀ + inner ℝ (gradient f x₀) (x - x₀) + (1 / 2) * hessianForm f x₀ (x - x₀)
-    := sorry
+    ∀ x, f x = f x₀ + inner ℝ (gradient f x₀) (x - x₀) + (1 / 2) * hessianQuadraticMap f x₀ (x - x₀)
+    := by
+    sorry
 
-example : MvPolynomial (Fin 2) ℝ := {
-    toFun := fun p => by
-        let c := p.toFun
-        let s := p.support
-        -- output: the coefficient of x^c 0 y^c 1
-        sorry
-    support           := sorry
-    mem_support_toFun := sorry
-}
+theorem getCoercive {n : ℕ}
+    {f : EuclideanSpace ℝ (Fin n) → ℝ} {x₀ : EuclideanSpace ℝ (Fin n)}
+    (C : ContinuousMultilinearMap ℝ (fun i : Fin 2 ↦ EuclideanSpace ℝ (Fin n)) ℝ)
+    (Q : QuadraticMap ℝ (EuclideanSpace ℝ (Fin n)) ℝ)
+    (hQQ : Q.PosDef) (ε : ℝ) (hε : 0 < ε) :
+    IsCoercive (continuousBilinearMap_of_continuousMultilinearMap
+        C) := by
+    let D := continuousBilinearMap_of_continuousMultilinearMap
+        C
+    use ε -- add hypotheses about ε
+    constructor
+    · exact hε
+    · intro u
+      unfold continuousBilinearMap_of_continuousMultilinearMap
+      unfold QuadraticMap.PosDef  at hQQ
+      simp at hQQ ⊢
+
+      sorry
+
+theorem getCoercive' {n : ℕ}
+    {f : EuclideanSpace ℝ (Fin n) → ℝ} {x₀ : EuclideanSpace ℝ (Fin n)}
+    (hQQ : (hessianQuadraticMap f x₀).PosDef) (ε : ℝ) (hε : 0 < ε) :
+    IsCoercive (continuousBilinearMap_of_continuousMultilinearMap
+        (iteratedFDeriv ℝ 2 f x₀)) := by
+    let C := iteratedFDeriv ℝ 2 f x₀
+    use ε -- add hypotheses about ε
+    constructor
+    · exact hε
+    · intro u
+      unfold continuousBilinearMap_of_continuousMultilinearMap
+      unfold QuadraticMap.PosDef hessianQuadraticMap at hQQ
+      simp at hQQ ⊢
+
+      sorry
